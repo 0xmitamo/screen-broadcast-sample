ea01f55b4d50539618cfbe677f1839fd7a1bc30d
Hironori Mitani <mitanihironori@gmail.com>
2022-01-25 15:56:16 +0900

    screen broadcast

---
+++ ScreenShare/SampleHandler.swift
@@ -0,0 +1,49 @@
+//
+//  SampleHandler.swift
+//  ScreenShare
+//
+//  Created by 三谷大典 on 2022/01/25.
+//
+
+import ReplayKit
+
+class SampleHandler: RPBroadcastSampleHandler {
+
+    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional. 
+    }
+    
+    override func broadcastPaused() {
+        // User has requested to pause the broadcast. Samples will stop being delivered.
+    }
+    
+    override func broadcastResumed() {
+        // User has requested to resume the broadcast. Samples delivery will resume.
+    }
+    
+    override func broadcastFinished() {
+        // User has requested to finish the broadcast.
+    }
+    
+    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+        switch sampleBufferType {
+        case RPSampleBufferType.video:
+//            if let weakSelf = self {
+//                weakSelf.bufferCopy = sampleBuffer
+//                weakSelf.lastSendTs = Int64(Date().timeIntervalSince1970 * 1000)
+//            }
+//            // Send video buffer
+//            AgoraUploader.sendVideoBuffer(sampleBuffer)
+            break
+        case RPSampleBufferType.audioApp:
+            // Handle audio sample buffer for app audio
+            break
+        case RPSampleBufferType.audioMic:
+            // Handle audio sample buffer for mic audio
+            break
+        @unknown default:
+            // Handle other sample buffer types
+            fatalError("Unknown type of sample buffer")
+        }
+    }
+}
--- screen-broadcast-sample/ViewController.swift
+++ screen-broadcast-sample/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let broadcastPicker = RPSystemBroadcastPickerView(frame: CGRect(x: view.frame.origin.x / 2, y: view.frame.origin.y / 2, width: 100, height: 100))
+
+        // 追加したBroadcast Upload ExtensionのBundleIDを設定
+        broadcastPicker.preferredExtension = "com.apple.broadcast-services-upload"
+        // マイクのオン/オフ表示を設定
+        broadcastPicker.showsMicrophoneButton = true
+
+        // デフォルト表示の二重丸アイコンを文字列に変更
+        for subview  in broadcastPicker.subviews {
+            let b = subview as! UIButton
+                b.setImage(nil, for: .normal)
+                b.setTitle("画面共有", for: .normal)
+                b.setTitleColor(.black, for: .normal)
+        }
+        view.addSubview(broadcastPicker)
+
     }
 
 
